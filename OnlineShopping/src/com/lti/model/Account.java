ab16d44a448b8851dd446a3598f998082a4ef9d1
Sarit Shekhar Gaine <saritgaine1997@gmail.com>
2020-10-16 09:51:33 +0530

    added accounts through br1

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,18 @@
+package com.lti.model;
+
+public class Account {
+     int balance;
+     String details;
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+}
